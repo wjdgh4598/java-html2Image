59f4dc5018db7f3618da0fd14db02e4d43d638f6
wjdgh4598 <wjdgh4598@naver.com>
2023-11-22 00:58:55 +0900

    자바 이미지 변환 테스트

--- src/main/java/com/testhtml/testhtml/controller/ApiController.java
+++ src/main/java/com/testhtml/testhtml/controller/ApiController.java
@@ -1,2 +1,38 @@
-package com.testhtml.testhtml.controller;public class ApiController {
+package com.testhtml.testhtml.controller;
+
+
+import com.testhtml.testhtml.service.testService;
+import gui.ava.html.link.LinkInfo;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class ApiController {
+
+    @Autowired
+    private testService testService;
+
+    @GetMapping("/test")
+    public String testApi(HttpServletRequest request) {
+
+        String strHtml = testService.getRendertoJsoup(request);
+
+        return strHtml;
+    }
+
+    @GetMapping("/testFile")
+    public List<gui.ava.html.link.LinkInfo> testFileApi(HttpServletRequest request) {
+
+        List<LinkInfo> link = testService.getFileRender(request);
+
+        return link;
+    }
+
+
 }
--- src/main/java/com/testhtml/testhtml/controller/HomeController.java
+++ src/main/java/com/testhtml/testhtml/controller/HomeController.java
@@ -1,2 +1,18 @@
-package com.testhtml.testhtml.controller;public class HomeController {
+package com.testhtml.testhtml.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String homeView() {
+        return "/home";
+    }
+
+    @GetMapping("/test")
+    public String testView() {
+        return "/test";
+    }
 }
