26ee7674db8eca8b00f31699231dd333e5a6aba1
wjdgh4598 <wjdgh4598@naver.com>
2023-11-22 00:57:49 +0900

    first commit

---
+++ src/main/java/com/testhtml/testhtml/controller/ApiController.java
@@ -0,0 +1,2 @@
+package com.testhtml.testhtml.controller;public class ApiController {
+}
---
+++ src/test/java/com/testhtml/testhtml/TestHtmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testhtml.testhtml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestHtmlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
